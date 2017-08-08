472b3fd21fadedbf5e0f05637be90ad8372a2e6b
Aelly Liu <laellyl@gmail.com>
2017-08-07 20:18:06 -0500

    add comments to routes - identifies various routes needed

---
+++ controllers/html-routes.js
@@ -0,0 +1,22 @@
+
+
+//TODO
+// route: / 
+// display homepage
+
+
+//TODO
+// route: /login 
+// display login page
+
+
+//TODO
+// route: /login 
+// post login info - check credentials
+// if credential is correct route to salerperson or manager's homepage
+//credeintial is wrong route to failure login 
+
+
+//TODO
+// route: /failed to log in 
+// display page to alert user log in failed.
