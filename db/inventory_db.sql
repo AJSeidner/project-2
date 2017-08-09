461d325ee732d25756072812b96807408ddecdea
Aelly Liu <laellyl@gmail.com>
2017-08-08 20:34:12 -0500

    finished orm.js, not  tested

--- db/inventory_db.sql
+++ db/inventory_db.sql
@@ -8,6 +8,7 @@ CREATE TABLE `employees` (
   `role` varchar(45) NOT NULL,
   `email` varchar(250) NOT NULL,
   `password` varchar(45) NOT NULL,
+  `regionCode` varchar(5) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -15,18 +16,19 @@ CREATE TABLE `inventory` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(200) NOT NULL,
   `stock_qty` int(11) DEFAULT '0',
-  `unit_price` decimal(5,0) DEFAULT '0',
+  `unit_price` decimal(10,5) DEFAULT '0.00000',
   `regionCode` varchar(10) NOT NULL,
   `category` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `soldItems` (
+CREATE TABLE `inventory_line` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `employeeId` int(11) NOT NULL,
   `inventoryId` int(11) NOT NULL,
-  `sold_qty` int(11) DEFAULT NULL,
+  `qty` int(11) NOT NULL,
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `txnType` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_idx` (`employeeId`),
   KEY `id_idx1` (`inventoryId`),
